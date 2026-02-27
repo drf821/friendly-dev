e8b84a1e956542dfa7ebf181616ac878de25ed1d
daniel faubel <drf821@gmail.com>
2026-02-27 14:16:16 -0700

    Add active link styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,9 @@ import { NavLink } from "react-router";
 import { FaLaptopCode } from "react-icons/fa";
 
 const Navbar = () => {
+const base = 'transition hover:text-blue-400';
+const active = 'text-blue-400 font-semibold';
+
     return ( 
         <nav className="bg-gray-800 border-gray-700 shadow-md sticky top-0 z-50">
             <div className="msx-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -11,11 +14,36 @@ const Navbar = () => {
                     {/* Dexktop Nav */}
                     <div className="hidden md:flex items-center gap-6">
                         <div className="space-x-4 text-sm text-gray-300">
-                            <NavLink to='/'>Home</NavLink>
-                            <NavLink to='/projects'>Projects</NavLink>
-                            <NavLink to='/blog'>Blog</NavLink>
-                            <NavLink to='/about'>About</NavLink>
-                            <NavLink to='/contact'>Contact</NavLink>
+                            <NavLink 
+                            className={({isActive}) => isActive ? active : base } 
+                            to='/'
+                            >
+                                Home
+                            </NavLink>
+                            <NavLink 
+                            className={({isActive}) => isActive ? active : base } 
+                            to='/projects'
+                            >
+                                Projects
+                            </NavLink>
+                            <NavLink 
+                            className={({isActive}) => isActive ? active : base } 
+                            to='/blog'
+                            >
+                                Blog
+                            </NavLink>
+                            <NavLink 
+                            className={({isActive}) => isActive ? active : base } 
+                            to='/about'
+                            >
+                                About
+                            </NavLink>
+                            <NavLink 
+                            className={({isActive}) => isActive ? active : base } 
+                            to='/contact'
+                            >
+                                Contact
+                            </NavLink>
                         </div>
                     </div>
                 </NavLink>
